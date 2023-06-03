15768af97327e6377cfe6464847d9f92ee1e36ce
arturpedrotti <67712554+arturpedrotti@users.noreply.github.com>
2023-06-03 11:13:21 -0300

    Update to display pictures of the integrantes, comparing the keywords inside the txt file to each sentence and putting them in bullet points if they match... and rename testandoPrograma.py to RaspagemECMI.py

--- RaspagemECMI.py
+++ RaspagemECMI.py
@@ -1,4 +1,3 @@
-# Importando classes do Selenium.
 import streamlit as st
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -6,54 +5,48 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
 import re
-# Configurando o webdriver
+
 def main():
     options = webdriver.FirefoxOptions()
-    options.add_argument("--headless")
-    # Utiliza o path padrao do geckodriver
-    driver = webdriver.Firefox(options=options)
-    # Site url
+    options.add_argument("--headless") # Garante que o GUI (interface do usuario) esta desligado
+    driver = webdriver.Firefox(options=options) # usa o path padrao do geckodriver
+
+
     url = "https://ecmi.fgv.br/corpo-docente"
-    # Navega até o site
-    driver.get(url)
+    driver.get(url) # Navega ate o site
     
-    # Espera até todos os elementos forem carregados.
     wait = WebDriverWait(driver, 10) # WebDriverWait esperará por um máximo de 10 segundos até jogar um TimeoutException. 20 vezes total pois ele checa a condição a cada 500ms por padrão.
 
-    teachers = {}
-    
+    teachers = list() # Criando uma lista vazia com list()
+
     # Acha qualquer elemento com as classes (. prefix) "field-content" e "mb-0" (o espaço em seguida instrui o selector para selecionar os elementes descendentes desse elemento (pois queremos sómente os cargos dos prof e nenhuma informação adicional) que estão dentro de uma tag anchor (a[]), em um attributo href que começa com (^=) o valor especificado. 
     elements = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".field-content.mb-0 a[href^='/integrante/']")))
-    
+
     # Criando um set (que guarda somente um valor único) vazio que é guardado dentro da variável unique_cargos
-    unique_cargos = set() 
+    unique_cargos = set()
     for element in elements:
         cargo = element.get_attribute('innerHTML').lower()
         unique_cargos.add(cargo)
 
     st.markdown("<h1 style='text-align: center; color: blue;'>FGV ECMI</h1>", unsafe_allow_html=True)
     
-    unique_cargos_list = ["Selecione o cargo desejado"] + list(unique_cargos)
-    # Cria um seletor de caixa suspensa (select box) para o usuário escolher o cargo
+    unique_cargos_list = ["Digite ou selecione um cargo"] + list(unique_cargos)
     cargoInput = st.selectbox("Cargo:", unique_cargos_list)
 
-    if cargoInput != "Selecione o cargo desejado":
+    if cargoInput != "Digite ou selecione um cargo":
         for element in elements:
             cargo = element.get_attribute('innerHTML')
-            # Verifica se o cargo selecionado está presente no nome do cargo
             if cargoInput in cargo.lower():
                 nome = element.get_attribute('href').split('/')[-1]
                 teachers[nome] = element.get_attribute('href')
 
-        teachers_names = ["Selecione um integrante"] + [name.replace("-", " ").title() for name in teachers.keys()]
-        # Cria um seletor de caixa suspensa (select box) para o usuário escolher o integrante
+        teachers_names = ["Digite ou selecione um integrante"] + [name.replace("-", " ").title() for name in teachers.keys()]
         nomeInput = st.selectbox("Integrante:", teachers_names)
 
-        if nomeInput != "Selecione um integrante":
+        if nomeInput != "Digite ou selecione um integrante":
             dados = []
             for nome, link in teachers.items():
                 nome = nome.replace("-", " ").title()
-                # Verifica se o nome do integrante selecionado está presente no nome do integrante
                 if nomeInput.lower() in nome.lower():
                     st.write(f"Raspando dados por {nome}...")
                     driver.get(link)
@@ -71,30 +64,35 @@ def main():
                     sentences = []
                     for para in infoPara:
                         texto = para.text
-                        # Usa expressões regulares para encontrar as sentenças que terminam com um ponto final
-                        sentences.extend(re.findall(r"(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s+([A-Z].*?\.)(?=\s+|$)", texto))
-                    
+                        sentences.extend(re.split(r'(?<=[.!?]) +', texto))
+
                     bullet_points = []
+                    keyword_found = False
                     for sentence in sentences:
                         if any(keyword in sentence.lower() for keyword in keywords):
-                            bullet_points.append(sentence.capitalize().strip())
+                            bullet_points.append(sentence.strip())
+                            keyword_found = True
+
+                    if not keyword_found:
+                        bullet_points = sentences
 
                     col1, col2 = st.columns(2)
                     with col1:
                         st.image(img_url)
                     with col2:
                         st.write(f"Informações sobre {nome}:")
-                        for bullet in set(bullet_points):  # Usa um conjunto para remover duplicatas
-                            st.write("- " + bullet)
+                        for bullet in bullet_points:
+                            st.write("- " + bullet.capitalize())
                         if email != "ecmi@fgv.br":
                             st.write(f"Email: {email}")
+                        st.write(f"Veja no site do corpo docente da ECMI: {link}")
                     
                     dados.append({"Nome": nome, "Sentenças": bullet_points, "Email": email})
 
             driver.quit()
-            # cria um dataframe da lista de dados, e escreve-a em um arq csv.
+            
             df = pd.DataFrame(dados)
             df.to_csv("scraped_data.csv", index=False)
 
-if __name__ == "__main__": # Uma prática boa para aplicações no streamlit, ele assegura que a função main() só é executada se o script é chamado diretamente (e não importado).
+if __name__ == "__main__":
     main()
